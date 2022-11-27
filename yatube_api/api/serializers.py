aa1f41308d59d74d545892a70ca5a30853d841e8
Eugene Miskiv <evgenymiskiv@gmail.com>
2022-11-27 14:41:25 +0300

    1

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
+from posts.models import Comment, Post, Follow, Group
 
-from posts.models import Comment, Post
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -13,6 +16,13 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
 
 
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username'
@@ -21,3 +31,20 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+
+    user = SlugRelatedField(slug_field='username', read_only=True, default=serializers.CurrentUserDefault())
+    following = SlugRelatedField(slug_field='username', queryset=User.objects.all())
+
+    class Meta:
+        fields = '__all__'
+        model = Follow
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following')
+            )
+        ]
+
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,55 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets, mixins, permissions, filters
+from rest_framework.generics import get_object_or_404
+from rest_framework.pagination import LimitOffsetPagination
+
+from api.permissions import IsAuthorOrReadOnly
+from api.serializers import PostSerializer, CommentSerializer, FollowSerializer, GroupSerializer
+from posts.models import Post, Comment, Follow, Group
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = IsAuthorOrReadOnly,
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    pagination_class = None
+    permission_classes = permissions.AllowAny,
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = IsAuthorOrReadOnly,
+    pagination_class = None
+    
+    def get_queryset(self):
+        return Comment.objects.filter(post=self.kwargs.get('post_id'))
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user,
+                        post=get_object_or_404(Post,
+                                               pk=self.kwargs.get('post_id')))
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = permissions.IsAuthenticated,
+    pagination_class = None
+    filter_backends = filters.SearchFilter,
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        return Follow.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
